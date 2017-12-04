6eabb2decf3ff30905e51f11c22943b2deaee5df
idoshhh <idoshahar@gmail.com>
2017-12-04 14:43:34 +0200

    Solution is ready to deploy

---
+++ Solution/js/aa/aajs.js
@@ -0,0 +1,197 @@
+﻿$(document).ready(function () {
+
+    var invertHandler = false;
+    var grayHandler = false;
+
+
+    var zoomText = function () {
+        return $('[data-aaZoom="true"]');
+    };
+
+    var underlineText = function () {
+        return $('[data-aaUnderline="true"]');
+    };
+
+    var zoomClicked = 0;
+    var zoomMax = 2;      //------------------------------------------------------ 1 click = 10% zoom text
+
+    $('.aa-icon-btn').on('click', function () {
+        var box = $('.aa-wrapper');
+        box.toggleClass('aa-right-non');
+    });
+    $('.aa-close').on('click', function () {
+        var box = $('.aa-wrapper');
+        box.toggleClass('aa-right-non');
+    });
+
+    $('.aa-text-plus').on('click', function () {
+
+        if (zoomClicked < zoomMax) {
+            zoomClicked++;
+            console.log('plus ' + zoomClicked);
+            var zoomFont = zoomText();
+            zoomFont.each(function () {
+                var size = parseFloat($(this).css('font-size'));
+                var currentSize = (size / 100) * 110;
+                $(this).css('font-size', currentSize + 'px')
+            })
+        }
+    });
+
+    $('.aa-text-minus').on('click', function () {
+
+        if (zoomClicked > 0) {
+            zoomClicked--;
+            console.log('minus ' + zoomClicked);
+            var zoomFont = zoomText();
+            zoomFont.each(function () {
+                var size = parseFloat($(this).css('font-size'));
+                var currentSize = (size / 100) * 90;
+                $(this).css('font-size', currentSize + 'px')
+            })
+        }
+
+    });
+
+    $('.aa-underlineEvent').on('click', function () {
+        var undeline = underlineText();
+        undeline.each(function () {
+            $(this).toggleClass('aa-underline');
+        });
+    });
+
+    $('.aa-resetEvent').on('click', function () {
+        var zoomFont = zoomText();
+        zoomFont.each(function () {
+            zoomFont.attr('style', '""')
+        });
+        var underline = underlineText();
+        underline.each(function () {
+            if ($(this).hasClass('aa-underline')) {
+                $(this).removeClass('aa-underline')
+            }
+        });
+
+        $('style[class="invert"]').each(function () {
+            $(this).remove();
+            invertHandler = false;
+        });
+
+        if ($('body').hasClass('aa-gray')) {
+            $('body').removeClass('aa-gray');
+            grayHandler = false;
+        }
+
+        if ($('body').hasClass('aa-readFont')) {
+            $('body').removeClass('aa-readFont')
+        }
+
+        if ($('body').hasClass('aa-textVersion')) {
+            $('body').removeClass('aa-textVersion')
+        }
+
+    });
+
+    $('.aa-pop-close, .aa-termsEvent').on('click', function () {
+        $('.aa-popup').toggleClass('aa-pop-display');
+    });
+
+    $('.aa-invertEvent').on('click', function () {
+
+        if (invertHandler == false && grayHandler == false) {
+            invertHandler = true;
+            invert();
+        } else if (invertHandler == true && grayHandler == false) {
+            invertHandler = false;
+            invert();
+        } else if (invertHandler == false && grayHandler == true) {
+            invertHandler = true;
+            invert();
+            grayHandler = false;
+            $('body').toggleClass('aa-gray')
+        } else {
+            invertHandler = false;
+            invert();
+            grayHandler = false;
+            $('body').toggleClass('aa-gray')
+        }
+
+
+    });
+
+    $('.aa-grayEvent').on('click', function () {
+        if (grayHandler == false && invertHandler == false) {
+            grayHandler = true;
+            $('body').toggleClass('aa-gray');
+        } else if (invertHandler == false && grayHandler == true) {
+            grayHandler = false;
+            $('body').toggleClass('aa-gray');
+        } else if (invertHandler == true && grayHandler == false) {
+            invertHandler = false;
+            invert();
+            grayHandler = true;
+            $('body').toggleClass('aa-gray')
+        } else {
+            invertHandler = false;
+            invert();
+            grayHandler = false;
+            $('body').toggleClass('aa-gray')
+        }
+
+    });
+
+    $('.aa-readFontEvent').on('click', function () {
+        $('body').toggleClass('aa-readFont')
+    });
+
+    $('.aa-clearColorsEvent').on('click', function () {
+
+        $('style[class="invert"]').each(function () {
+            $(this).remove();
+            invertHandler = false;
+        });
+
+        if ($('body').hasClass('aa-gray')) {
+            $('body').removeClass('aa-gray');
+            grayHandler = false;
+        }
+    });
+
+    $('.aa-textVersionEvent').on('click', function () {
+        $('body').toggleClass('aa-textVersion')
+    })
+
+});
+
+function invert() {
+// the css we are going to inject
+    var css = 'html {-webkit-filter: invert(100%);' +
+            '-moz-filter: invert(100%);' +
+            '-o-filter: invert(100%);' +
+            '-ms-filter: invert(100%); }',
+
+        head = document.getElementsByTagName('head')[0],
+        style = document.createElement('style');
+    style.setAttribute('class', 'invert');
+
+// a hack, so you can "invert back" clicking the bookmarklet again
+    if (!window.counter) {
+        window.counter = 1;
+    } else {
+        window.counter++;
+        if (window.counter % 2 == 0) {
+            var css = 'html {-webkit-filter: invert(0%); -moz-filter:    invert(0%); -o-filter: invert(0%); -ms-filter: invert(0%); }'
+        }
+    }
+    ;
+
+    style.type = 'text/css';
+    if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+    } else {
+        style.appendChild(document.createTextNode(css));
+    }
+
+//injecting the css to the head
+    head.appendChild(style);
+}
---
+++ Solution/js/script.js
@@ -0,0 +1,214 @@
+﻿var fields = [
+    ["#input_name", "שם מלא:", "^[\\sa-zA-Z\u0590-\u05FF\b ']+$"],
+    ["#input_phone", 'טלפון:', "^[\b0-9]+$"],
+    ["#input_email", 'מייל:', "^[\\-a-zA-Z0-9@._\u0590-\u05FF\b]+$"]
+];
+
+function getQueryVariable(variable) {
+    "use strict";
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i = 0; i < vars.length; i++) {
+        var pair = vars[i].split("=");
+        if (pair[0] === variable) {
+            return pair[1];
+        }
+    }
+    return "nosource";
+}
+
+function reportLead() {
+
+    "use strict";
+    var url = "http://crmleads.si-mark.com/mcm/datao/lead?externalURL=true";
+    var groupID = "4008";
+    var siteID = "57";
+    var msp = "P09TI01R548VPK4L";
+    var uName = $("#input_name").val();
+    var uPhone = $("#input_phone").val();
+    var uEmail = $("#input_email").val();
+    var uDivur = $('#check-box').is(':checked');
+    var uRef = getQueryVariable("utm_source");
+    var campaignName = "december2017";
+    var imgURL = encodeURI(url + '&groupID=' + groupID + '&siteID=' + siteID + '&msp=' + msp + '&uName=' + uName + '&uPhone=' + uPhone + '&uEmail=' + uEmail + '&general1=' + uDivur + '&general2=' + uRef + '&general3=' + campaignName);
+    var i = document.createElement("img");
+    i.src = imgURL;
+
+    if (getQueryVariable('utm_source') === 'yad2') {
+
+        var pxls = $('.pxls');
+        var html = '<script src="http://q.mimgoal.com/l/?mdscampaignid=5912e29363135833635686&mdschannelid=' + getQueryVariable('mdschannelid') + '&mdsviewid=' + getQueryVariable('mdsviewid') + '&fname=' + uName + '&lname=' + '' + '&email=' + uEmail + '&phone=' + uPhone + '" type="text/javascript"></script><noscript><img src="http://q.mimgoal.com/l/?mdscampaignid=5912e29363135833635686&mdschannelid=' + getQueryVariable('mdschannelid') + '&mdsviewid=' + getQueryVariable('mdsviewid') + '&fname=' + uName + '&lname=' + '' + '&email=' + uEmail + '&phone=' + uPhone + '" width="1" height="1" /></noscript>';
+        pxls.html(html);
+
+    }
+
+    var url2 = "http://www1.tami4.co.il/campaigns/sendlead";
+    var imgURL2 = encodeURI(url2 + '&name=' + uName + '&phone=' + uPhone + '&mail=' + uEmail + '&newsletter=' + uDivur + '&campaignName=' + "campaign_March_17" + '&landingUrl=' + window.location.href + '&providerName=' + "Adler" + '&affiliateid=' + getQueryVariable("utm_source") + '&refName=' + getQueryVariable("utm_source") + '&ref=' + getQueryVariable("utm_source") + '&bantype=' + getQueryVariable("utm_source") + '&banid=' + "3144");
+    var i2 = document.createElement("img");
+    i2.src = imgURL2;
+}
+
+
+function validateForm() {
+    "use strict";
+    $(".error").html('');
+    $("#input_email").removeClass('input-error');
+    $("#input_name").removeClass('input-error');
+    $("#input_phone").removeClass('input-error');
+    if (!isValidName($("#input_name").val()) || $("#input_name").val() === fields[0][1]) {
+        $(".error").html("שם מלא - שדה חובה");
+        $("#input_name").focus();
+        $("#input_name").addClass('input-error');
+        return false;
+    }
+    if (!isValidPhone($("#input_phone").val()) || $("#input_phone").val() === fields[1][1]) {
+        $(".error").html("שדה טלפון - שדה חובה");
+        $("#input_phone").focus();
+        $("#input_phone").addClass('input-error');
+        return false;
+    }
+    if ($("#input_email").val() !== '') {
+        if (!isValidEmailAddress($("#input_email").val())) {
+            $(".error").html('שדה מייל - שדה חובה');
+            $("#input_email").focus();
+            $("#input_email").addClass('input-error');
+            return false;
+        }
+    }
+
+    return true;
+}
+
+function isValidName(name) {
+    "use strict";
+    var pattern1 = new RegExp(/^[a-zA-Z\.\u0590-\u05FF \s']{1,}$/i);
+    return pattern1.test(name);
+}
+
+function isValidPhone(phone) {
+    "use strict";
+    var pattern = new RegExp(/^0+(50|52|53|54|55|56|58|59|2|3|4|8|9|72|73|74|76|77|79)[1-9]{1}[0-9]{6}$/i);
+    return pattern.test(phone);
+}
+
+
+function isValidEmailAddress(emailAddress) {
+    "use strict";
+    var pattern = new RegExp(/^[+a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/);
+    return pattern.test(emailAddress);
+}
+
+function minH() {
+    // if( window.innerWidth > 1000 ){
+    //     if( window.innerHeight < 780 ){
+    //         if( window.innerWidth < 1100 ){
+    //             $('.wrapper').css('min-height', window.innerHeight+'px');
+    //         }else {
+    //             $('.wrapper').css('min-height', '780px');
+    //         }
+    //     }else {
+    //         $('body').css('min-height', window.innerHeight+'px');
+    //     }
+    // }
+}
+
+
+function doFields(fld) {
+    "use strict";
+    $(fld[0]).focus(function () {
+        if ($(this).val() === fld[1]) {
+            $(this).val('').removeClass("placeholder");
+        }
+    });
+
+    $(fld[0]).blur(function () {
+        var newValue = $(this).val();
+        if (newValue === '' || newValue === fld[1]) {
+            $(this).val(fld[1]).addClass("placeholder");
+        }
+    }).triggerHandler('blur');
+
+    $(fld[0]).bind('keypress', function (event) {
+        var regex = new RegExp(fld[2]);
+        var key = String.fromCharCode(!event.charCode ? event.which : event.charCode);
+        if (!regex.test(key)) {
+            event.preventDefault();
+            return false;
+        }
+    });
+}
+
+$(document).ready(function () {
+
+    minH();
+    $(window).on('resize', minH);
+
+    $('#submit').on('click', function (e) {
+        e.preventDefault();
+        if (validateForm()) {
+            //$('.thanks').fadeIn(500);
+            $('.container').addClass('fade-out');
+            setTimeout(function () { $('.wrapper').addClass('wrapper-thanks'); }, 500);
+
+            reportLead();
+            $('#pxl').attr('src', 'thanks_pxl.html');
+        }
+
+    });
+
+    if (getQueryVariable("utm_source") === 'facebook') {
+        $('.c2c').attr('href', 'tel:072-330-8374')
+    }
+
+
+    var date = new Date();
+    var day = date.getDay();
+    var hour = date.getHours();
+    var minute = date.getMinutes();
+    var currentDay = date.getDate();
+    var month = date.getMonth();
+    var showForm = true;
+
+    if (day >= 0 && day <= 5) {
+        if (day === 5 && ((hour >= 8 && minute >= 30) || hour >= 9) && hour <= 11) {
+            showForm = false;
+            console.log('friday day');
+        } else if (day < 5 && ((hour >= 8 && minute >= 30) || hour >= 9) && hour <= 17) {
+            showForm = false;
+        } else {
+            showForm = true;
+        }
+    } else if (day === 6) {
+        showForm = true;
+    }
+
+    if (showForm) {
+        if ($(window).width() < 1000) {
+            $('.form').css('display', 'block');
+            $('.mobile_btns').css('display', 'none');
+        }
+    }
+
+    $('.open-form').on('click', function () {
+        //$('.form').css('display', 'block');
+        //$('.mobile_btns').css('display', 'none');
+        $('.mobile_btns').hide();
+        $('.form').fadeIn(500);
+
+    });
+
+    // $('.c2c').on('click', function () {
+    //     $('#pxl').attr('src', 'c2c_pxl.html')
+    // })
+
+});
+
+function lettersOnly(evt) {
+    evt = (evt) ? evt : event;
+    var charCode = (evt.charCode) ? evt.charCode : ((evt.keyCode) ? evt.keyCode : ((evt.which) ? evt.which : 0));
+    if (charCode > 31 && (charCode < 65 || charCode > 90) && (charCode < 97 || charCode > 122)) {
+        return false;
+    } else {
+        return true;
+    }
+}
